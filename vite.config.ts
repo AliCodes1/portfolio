c62ea8ecd249a22d94a644cb1857e1fc7b9f8ae2
Muhammad Ali <mali42@uoguelph.ca>
2025-01-05 19:56:33 -0500

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/landingpage/', // replace with your repository name
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
+  base: '/portfolio/', 
 }) 
